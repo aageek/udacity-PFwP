4df18fa1a48a81b0642f0f122575d864dc2239be
Abhishek Anand <aanandmailbox@gmail.com>
2015-08-30 21:52:09 -0700

    Adding Prank folder and List of all files in that folder

--- rename_files.py
+++ rename_files.py
@@ -1,7 +1,8 @@
-
+import os
 def rename_files():
     # (1) Get the file names from a folder
-
+    file_list = os.listdir(r"/Users/Anand/GitHub/udacity-PFwP/prank")
+    print (file_list)
     # (2) for each file, rename filename
 
 rename_files()
