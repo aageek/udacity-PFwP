d6641f5b5d21ad62b8ef81773a31f3805b61eb6a
Abhishek Anand <aanandmailbox@gmail.com>
2015-08-30 21:30:28 -0700

    Adding Start time and End Time

--- break_time.py
+++ break_time.py
@@ -4,9 +4,11 @@ import webbrowser
 total_break = 3
 break_count = 0
 
-
-while(break_count < total_break):
+print ("This Program started on "+ time.ctime())
+while(break_count < total_break) :
     time.sleep(1)
     webbrowser.open("http://www.youtube.com/watch?v=iJllh7l-D3g")
 
     break_count = break_count + 1
+
+print ("This Program finished on "+ time.ctime())
